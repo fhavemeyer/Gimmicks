fe5aa6b4ab1e110a157b881febd01d8b326232ea
TeaJizzle <tom@tomjervis.co.uk>
2014-02-19 20:38:33 +0000

    Quick /team implementation.

--- src/com/gimmicknetwork/gimmicks/commands.java
+++ src/com/gimmicknetwork/gimmicks/commands.java
@@ -1,8 +1,11 @@
 package com.gimmicknetwork.gimmicks;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.kitteh.tag.TagAPI;
 
 import com.gimmicknetwork.gimmicks.gimmicks;
 
@@ -16,7 +19,41 @@ public class commands implements CommandExecutor
   
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
-		
+		if (cmd.getName().equalsIgnoreCase("team")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("Only players can run this command");
+			} else {
+				Player p = (Player) sender;
+				if (args.length > 0) {
+		        	switch (args[0]) {
+		        	
+		        	case "red":		this.plugin.teams.put(p.getName(), ChatColor.RED);
+		        					sender.sendMessage("Joined team: " + ChatColor.RED + "RED");
+		        					break;
+		        	case "blue":	this.plugin.teams.put(p.getName(), ChatColor.BLUE);
+		        					sender.sendMessage("Joined team: " + ChatColor.BLUE + "BLUE");
+									break;
+		        	case "green":	this.plugin.teams.put(p.getName(), ChatColor.GREEN);
+		        					sender.sendMessage("Joined team: " + ChatColor.GREEN + "GREEN");
+									break;
+		        	case "purple":	this.plugin.teams.put(p.getName(), ChatColor.LIGHT_PURPLE);
+		        					sender.sendMessage("Joined team: " + ChatColor.LIGHT_PURPLE + "PURPLE");
+									break;
+		        	case "gold":	this.plugin.teams.put(p.getName(), ChatColor.GOLD);
+		        					sender.sendMessage("Joined team: " + ChatColor.GOLD + "GOLD");
+									break;
+		        	case "aqua":	this.plugin.teams.put(p.getName(), ChatColor.AQUA);
+		        					sender.sendMessage("Joined team: " + ChatColor.AQUA + "AQUA");
+									break;
+		        	default:		this.plugin.teams.remove(p.getName());
+		        					sender.sendMessage("You left your team.");
+		        					break;
+		        	}
+		        	TagAPI.refreshPlayer(p);
+				}
+			}
+			
+		}
 		
 		return true;
 	}
--- src/com/gimmicknetwork/gimmicks/gimmicks.java
+++ src/com/gimmicknetwork/gimmicks/gimmicks.java
@@ -1,7 +1,39 @@
 package com.gimmicknetwork.gimmicks;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
 
-public final class gimmicks extends JavaPlugin {
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.kitteh.tag.AsyncPlayerReceiveNameTagEvent;
+
+public final class gimmicks extends JavaPlugin implements Listener {
+	//set up hashmap for teams
+	public HashMap<String, ChatColor> teams = new HashMap<String, ChatColor>();
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("[CivpvpInventory] plugin enabled!");
+		getCommand("team").setExecutor(new commands(this));
+		this.saveDefaultConfig();
+		
+	}
+	
+	public void onDisable() {
+		getLogger().info("[CivpvpInventory] plugin enabled!");
+	}
+	
+	@EventHandler(priority=EventPriority.NORMAL)
+	public void onNameTag(AsyncPlayerReceiveNameTagEvent event) {
+		String name = event.getNamedPlayer().getName().toString();
+		ChatColor cc = teams.get(name);
+		getLogger().info("NAMETAG EVENT CALLED");
+		if (cc != null) {
+			event.setTag(cc + name);
+		} else {
+			event.setTag(name);
+		}
+	}
 	
 }
