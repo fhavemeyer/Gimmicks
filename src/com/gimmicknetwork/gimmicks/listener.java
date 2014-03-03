d7f1be782edf9d1b83c08e068e90472fe7671c7a
TeaJizzle <tom@tomjervis.co.uk>
2014-03-03 15:05:17 +0000

    Actually made the death message enabled config work.

--- src/com/gimmicknetwork/gimmicks/listener.java
+++ src/com/gimmicknetwork/gimmicks/listener.java
@@ -87,10 +87,8 @@ public class listener implements Listener {
 		}		
 	}
 
-	//death announce
 	
-	
-	//memcache player faces on join
+	//load player faces into memory on join
 	@EventHandler
 	public void onAsyncPlayerLogin(AsyncPlayerPreLoginEvent event) {
 		
@@ -125,48 +123,51 @@ public class listener implements Listener {
 				}
 			}
 			
-			//THUNDER STRUCK, JUST LOOK AT THE MESS YOU MADE
-			p.getWorld().strikeLightningEffect(p.getLocation().add(0, 100, 0));
-			if (gimmicks.faceCache.get(p.getName().toString()) != null){
-				BufferedImage imageToSend;			
-				imageToSend = (BufferedImage) gimmicks.faceCache.get(p.getName().toString());
-				String slainMsg = p.getName().toString() + " was slain.";
-				String killerMsg = "";
-				String extraMsg = "Good night, sweet prince.";
+			if (gimmicks.getConfig().getBoolean("facedeathmessages.enabled", true)) {
+				//THUNDER STRUCK, JUST LOOK AT THE MESS YOU MADE
+				p.getWorld().strikeLightningEffect(p.getLocation().add(0, 100, 0));
 				
-				//slain messages
-				if (p.getKiller() != null && p.getKiller() instanceof Player) {
-					slainMsg = p.getName().toString() + " was slain by " + p.getKiller().getName() + ".";
-					Player killer = (Player) p.getKiller();
+				//start making our fancy death message
+				if (gimmicks.faceCache.get(p.getName().toString()) != null){
+					BufferedImage imageToSend;			
+					imageToSend = (BufferedImage) gimmicks.faceCache.get(p.getName().toString());
+					String slainMsg = p.getName().toString() + " was slain.";
+					String killerMsg = "";
+					String extraMsg = "Good night, sweet prince.";
 					
-					//kill streak of killer
-					if ( gimmicks.killStreak.get(p.getKiller().getName()) != null) {
-						int killerKillStreak = gimmicks.killStreak.get(killer.getName()) + 1;
-						gimmicks.killStreak.put(killer.getName(), killerKillStreak);
-						killerMsg = killer.getName() + " has a killstreak of " + Integer.toString(killerKillStreak);
-						if (killerKillStreak >= 5) { extraMsg = killer.getName() + " is " + ChatColor.RED + "DOMINATING!"; }
-						if (killerKillStreak >= 10) { extraMsg = killer.getName() + " is on a " + ChatColor.RED + ChatColor.BOLD + "RAMPAGE!"; }
-						if (killerKillStreak >= 15) { extraMsg = killer.getName() + " is " + ChatColor.AQUA + ChatColor.BOLD + "GOD-LIKE!"; }
-					} else {
-						gimmicks.killStreak.put(killer.getName(), 1);
-						killerMsg = killer.getName() + " now has a killstreak of 1";
+					//slain messages
+					if (p.getKiller() != null && p.getKiller() instanceof Player) {
+						slainMsg = p.getName().toString() + " was slain by " + p.getKiller().getName() + ".";
+						Player killer = (Player) p.getKiller();
+						
+						//kill streak of killer
+						if ( gimmicks.killStreak.get(p.getKiller().getName()) != null) {
+							int killerKillStreak = gimmicks.killStreak.get(killer.getName()) + 1;
+							gimmicks.killStreak.put(killer.getName(), killerKillStreak);
+							killerMsg = killer.getName() + " has a killstreak of " + Integer.toString(killerKillStreak);
+							if (killerKillStreak >= 5) { extraMsg = killer.getName() + " is " + ChatColor.RED + "DOMINATING!"; }
+							if (killerKillStreak >= 10) { extraMsg = killer.getName() + " is on a " + ChatColor.RED + ChatColor.BOLD + "RAMPAGE!"; }
+							if (killerKillStreak >= 15) { extraMsg = killer.getName() + " is " + ChatColor.AQUA + ChatColor.BOLD + "GOD-LIKE!"; }
+						} else {
+							gimmicks.killStreak.put(killer.getName(), 1);
+							killerMsg = killer.getName() + " now has a killstreak of 1";
+						}
 					}
 					
+					//remove killstreak
+					if (gimmicks.killStreak.get(p.getName()) != null) {
+						gimmicks.killStreak.remove(p.getName());
+					}
+					
+					new ImageMessage(imageToSend, 8, ImageChar.BLOCK.getChar()).appendText(
+							 "",
+							 "",
+							 "",
+							 slainMsg,
+							 killerMsg,
+							 extraMsg
+							 ).sendToPlayers(Bukkit.getOnlinePlayers());
 				}
-				
-				//remove killstreak
-				if (gimmicks.killStreak.get(p.getName()) != null) {
-					gimmicks.killStreak.remove(p.getName());
-				}
-				
-				new ImageMessage(imageToSend, 8, ImageChar.BLOCK.getChar()).appendText(
-						 "",
-						 "",
-						 "",
-						 slainMsg,
-						 killerMsg,
-						 extraMsg
-						 ).sendToPlayers(Bukkit.getOnlinePlayers());
 			}
 		}
 	}
